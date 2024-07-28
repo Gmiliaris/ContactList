4629d838770b8ac87305942e53e9cfdbc9fd3274
Gmiliaris <miliarisgustavo@gmail.com>
2024-07-28 15:59:35 +0100

    developed the new activity about the detail information to share the contact

--- app/src/main/java/com/devspace/recyclerview/ContactDetailActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/ContactDetailActivity.kt
@@ -1,6 +1,9 @@
 package com.devspace.recyclerview
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,30 @@ class ContactDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_contact_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val tvname = findViewById<TextView>(R.id.tv_name)
+        val tvphone = findViewById<TextView>(R.id.tv_phone)
+        val image = findViewById<ImageView>(R.id.image)
+        val tvShare = findViewById<TextView>(R.id.tv_share_contact)
+
+        val name = intent.getStringExtra("name")
+        val phone = intent.getStringExtra("phone")
+        val icon = intent.getIntExtra("icon", R.drawable.sample1)
+
+        tvname.text = name
+        tvphone.text = phone
+        image.setImageResource(icon)
+
+        tvShare.setOnClickListener {
+
+            val sendIntent : Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "$name $phone")
+                type = "text/plain"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
         }
     }
 }

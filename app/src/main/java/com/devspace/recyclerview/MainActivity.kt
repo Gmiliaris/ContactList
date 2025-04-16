6cf9452485438916d5077986d33210ccb1c1cc51
gustavomiliaris <miliarisgustavo@gmail.com>
2025-04-16 22:48:11 +0100

    add drawable for images of contacts and implemented data class and Adapter

--- app/src/main/java/com/devspace/recyclerview/MainActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,103 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val rvList = findViewById<RecyclerView>(R.id.rv_list)
+        val adapter = ContactListAdapter()
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+        adapter.submitList(contacts)
     }
+
 }
+
+val contacts = listOf(
+    Contact(
+        "John Doe",
+        "+353 85 123 4567",
+        R.drawable.sample14
+    ),
+
+    Contact(
+        "Vanessa Miller",
+        "+353 87 123 4567",
+        R.drawable.sample3
+    ),
+
+    Contact(
+        "Camila Mendes",
+        "+353 83 126 4567",
+        R.drawable.sample15
+    ),
+
+    Contact(
+        "Liam O'Sullivan",
+        "+353 86 234 5678",
+        R.drawable.sample9
+    ),
+
+    Contact(
+        "Megan Ryan",
+        "+353 87 234 5678",
+        R.drawable.sample13
+    ),
+
+    Contact(
+        "Emma Gallagher",
+        "+353 89 567 8901",
+        R.drawable.sample7
+    ),
+
+    Contact(
+        "Juliana Gomes ",
+        "+353 85 890 1234",
+        R.drawable.sample1
+    ),
+
+    Contact(
+        "Pedro Almeida",
+        "+353 89 567 8901",
+        R.drawable.sample8
+    ),
+
+    Contact(
+        "Carla Rocha",
+        "+353 87 678 9012",
+        R.drawable.sample16
+    ),
+
+    Contact(
+        "Vanessa Cardoso",
+        "+353 83 253 4567",
+        R.drawable.sample6
+    ),
+
+    Contact(
+        "Eduardo Lima",
+        "+353 83 143 4567",
+        R.drawable.sample10
+    ),
+
+    Contact(
+        "Renata Nunes",
+        "+353 83 162 4567",
+        R.drawable.sample4
+    ),
+
+    Contact(
+        "Roisin Moore",
+        "+353 143 4567",
+        R.drawable.sample11
+    ),
+
+    Contact(
+        "Roque Buarque",
+        "+353 89 125 4567",
+        R.drawable.sample12
+    ),
+
+    Contact(
+        "Bruna Costa",
+        "+353 86 126 4567",
+        R.drawable.sample5
+    ),
+)

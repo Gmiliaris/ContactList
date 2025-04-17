b8ea55be5e11549377afce138f89fb1791208954
gustavomiliaris <miliarisgustavo@gmail.com>
2025-04-17 22:21:40 +0100

    add new activity for detail of contacts and fixed news strings resources

--- app/src/main/java/com/devspace/recyclerview/MainActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.devspace.recyclerview
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -19,102 +22,121 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         val rvList = findViewById<RecyclerView>(R.id.rv_list)
+        val ivList = findViewById<ImageView>(R.id.iv_list)
+        val ivGrid = findViewById<ImageView>(R.id.iv_grid)
         val adapter = ContactListAdapter()
+
         rvList.adapter = adapter
         rvList.layoutManager = LinearLayoutManager(this)
         adapter.submitList(contacts)
+
+        ivGrid.setOnClickListener {
+            rvList.layoutManager = GridLayoutManager(this, 2)
+        }
+
+        ivList.setOnClickListener {
+            rvList.layoutManager = LinearLayoutManager(this)
+        }
+
+        adapter.setOnClickListener { contact ->
+            val intent = Intent(this, ContactDetailActivity::class.java)
+            intent.putExtra("name", contact.name)
+            intent.putExtra("phone", contact.phone)
+            intent.putExtra("icon", contact.icon)
+            startActivity(intent)
+        }
+
     }
 
+    val contacts = listOf(
+        Contact(
+            name = "John Doe",
+            phone = "+353 85 123 4567",
+            icon = R.drawable.sample14
+        ),
+
+        Contact(
+            name = "Vanessa Miller",
+            phone = "+353 87 123 4567",
+            icon = R.drawable.sample3
+        ),
+
+        Contact(
+            name = "Camila Mendes",
+            phone = "+353 83 126 4567",
+            icon = R.drawable.sample15
+        ),
+
+        Contact(
+            name = "Liam O'Sullivan",
+            phone = "+353 86 234 5678",
+            icon = R.drawable.sample9
+        ),
+
+        Contact(
+            name = "Megan Ryan",
+            phone = "+353 87 234 5678",
+            icon = R.drawable.sample13
+        ),
+
+        Contact(
+            name = "Emma Gallagher",
+            phone = "+353 89 567 8901",
+            icon = R.drawable.sample7
+        ),
+
+        Contact(
+            name = "Juliana Gomes ",
+            phone = "+353 85 890 1234",
+            icon = R.drawable.sample1
+        ),
+
+        Contact(
+            name = "Pedro Almeida",
+            phone = "+353 89 567 8901",
+            icon = R.drawable.sample8
+        ),
+
+        Contact(
+            name = "Carla Rocha",
+            phone = "+353 87 678 9012",
+            icon = R.drawable.sample16
+        ),
+
+        Contact(
+            name = "Vanessa Cardoso",
+            phone = "+353 83 253 4567",
+            icon = R.drawable.sample6
+        ),
+
+        Contact(
+            name = "Eduardo Lima",
+            phone = "+353 83 143 4567",
+            icon = R.drawable.sample10
+        ),
+
+        Contact(
+            name = "Renata Nunes",
+            phone = "+353 83 162 4567",
+            icon = R.drawable.sample4
+        ),
+
+        Contact(
+            name = "Roisin Moore",
+            phone = "+353 143 4567",
+            icon = R.drawable.sample11
+        ),
+
+        Contact(
+            name = "Roque Buarque",
+            phone = "+353 89 125 4567",
+            icon = R.drawable.sample12
+        ),
+
+        Contact(
+            name = "Bruna Costa",
+            phone = "+353 86 126 4567",
+            icon = R.drawable.sample5
+        ),
+    )
 }
-
-val contacts = listOf(
-    Contact(
-        "John Doe",
-        "+353 85 123 4567",
-        R.drawable.sample14
-    ),
-
-    Contact(
-        "Vanessa Miller",
-        "+353 87 123 4567",
-        R.drawable.sample3
-    ),
-
-    Contact(
-        "Camila Mendes",
-        "+353 83 126 4567",
-        R.drawable.sample15
-    ),
-
-    Contact(
-        "Liam O'Sullivan",
-        "+353 86 234 5678",
-        R.drawable.sample9
-    ),
-
-    Contact(
-        "Megan Ryan",
-        "+353 87 234 5678",
-        R.drawable.sample13
-    ),
-
-    Contact(
-        "Emma Gallagher",
-        "+353 89 567 8901",
-        R.drawable.sample7
-    ),
-
-    Contact(
-        "Juliana Gomes ",
-        "+353 85 890 1234",
-        R.drawable.sample1
-    ),
-
-    Contact(
-        "Pedro Almeida",
-        "+353 89 567 8901",
-        R.drawable.sample8
-    ),
-
-    Contact(
-        "Carla Rocha",
-        "+353 87 678 9012",
-        R.drawable.sample16
-    ),
-
-    Contact(
-        "Vanessa Cardoso",
-        "+353 83 253 4567",
-        R.drawable.sample6
-    ),
-
-    Contact(
-        "Eduardo Lima",
-        "+353 83 143 4567",
-        R.drawable.sample10
-    ),
-
-    Contact(
-        "Renata Nunes",
-        "+353 83 162 4567",
-        R.drawable.sample4
-    ),
-
-    Contact(
-        "Roisin Moore",
-        "+353 143 4567",
-        R.drawable.sample11
-    ),
-
-    Contact(
-        "Roque Buarque",
-        "+353 89 125 4567",
-        R.drawable.sample12
-    ),
-
-    Contact(
-        "Bruna Costa",
-        "+353 86 126 4567",
-        R.drawable.sample5
-    ),
-)
